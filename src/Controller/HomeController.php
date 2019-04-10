d1c0a62a581174739f67a9e902d13d3a5dec5422
david keney <davidmyken@gmail.com>
2019-04-10 23:06:09 +0200

    creating route with yaml and annotation

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,28 @@
+<?php
+namespace  App\Controller;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Twig\Environment;
+
+
+class HomeController{
+    /**
+     * @var Environment
+     */
+
+    private  $twig;
+
+    public function __construct(Environment $twig){
+        $this->twig = $twig;
+    }
+    /**
+     * @Route("/", name="home")
+     * @return Response
+     * @throws  \Twig_Error_Loader
+     * @throws  \Twig_Error_Runtime
+     * @throws  \Twig_Error_Syntax
+     */
+    public function index(): Response{
+        return new Response($this->twig->render('pages\home.html.twig'));
+    }
+}
