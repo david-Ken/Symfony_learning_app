a62a693cdcdb8dd09638816f267c9b6310dd7e2a
david keney <davidmyken@gmail.com>
2019-04-13 21:47:43 +0200

    setup database using doctrine

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -1,11 +1,12 @@
 <?php
 namespace  App\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Twig\Environment;
 
 
-class HomeController{
+class HomeController extends  AbstractController {
     /**
      * @var Environment
      */
@@ -23,6 +24,6 @@ class HomeController{
      * @throws  \Twig_Error_Syntax
      */
     public function index(): Response{
-        return new Response($this->twig->render('pages\home.html.twig'));
+        return $this->render('pages\home.html.twig');
     }
 }
--- src/Controller/PropertyController.php
+++ src/Controller/PropertyController.php
@@ -1,16 +1,17 @@
 <?php
 namespace  App\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 
-class PropertyController{
+class PropertyController extends  AbstractController {
     /**
      * @Route("/biens", name="property.index")
      * @return Response
      */
 
     public function index(): Response{
-        return new Response('les biens');
+          return $this->render('property/index.html.twig', ['current_menu'=> 'properties']);
     }
 }
