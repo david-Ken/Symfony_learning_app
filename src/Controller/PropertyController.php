f96709f231d9d37d1ff80af6dd4ec403f04bc4be
david keney <davidmyken@gmail.com>
2019-04-14 21:11:13 +0200

    addind data to database

--- src/Controller/PropertyController.php
+++ src/Controller/PropertyController.php
@@ -1,17 +1,40 @@
 <?php
-namespace  App\Controller;
+
+namespace App\Controller;
+
+use phpDocumentor\Reflection\DocBlock\Tags\Property;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 
-class PropertyController extends  AbstractController {
+class PropertyController extends AbstractController
+{
     /**
      * @Route("/biens", name="property.index")
      * @return Response
      */
 
-    public function index(): Response{
-          return $this->render('property/index.html.twig', ['current_menu'=> 'properties']);
+    public function index(): Response
+    {
+
+        $property = new Property();
+        $property->setTitle('Mon premier bien')
+            ->setPrice(200000)
+            ->setRooms(4)
+            ->setBedrooms(3)
+            ->setDescription('Une petite description')
+            ->setFloor(4)
+            ->setSurface(60)
+            ->setHeat(1)
+            ->setCity('Clermont')
+            ->setAddress('12 rue bernard brunhes')
+            ->setPostalCode('63000');
+
+       $em = $this->getDoctrine()->getManager();
+       $em->persist($property);
+       $em->flush();
+
+        return $this->render('property/index.html.twig', ['current_menu' => 'properties']);
     }
 }
